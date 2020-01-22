19f7557a5453d2c5cbb905bafd87fb03d648a0a6
Wanja Pflüger <wanja.friedemann.pflueger@fork.de>
2020-01-22 14:46:28 +0100

    # updated info script

--- scripts/info/info.bash
+++ scripts/info/info.bash
@@ -8,9 +8,21 @@ echo
 echo "  ${BLUE}TASK${NC} Display project information"
 echo
 
-# try craft
-$my_dir/../craft/info.bash
+ok=false
 
-echo
-echo "  ${GREEN}SUCCESS${NC} Done"
-echo
+# try craft
+if [ -d "site/config" ]; then
+  $my_dir/../craft/info.bash
+  ok=true
+fi
+
+if [ "$ok" = true ]; then
+  echo
+  echo "  ${GREEN}SUCCESS${NC} Done"
+  echo
+else
+  echo
+  echo "  ${RED}ERROR${NC} Could not find a method to execute command"
+  echo
+fi
+
