62f791b5109dc3e4212d2f962df55b6e35620434
Wanja Pflüger <wanja.friedemann.pflueger@fork.de>
2020-01-30 14:12:39 +0100

    + db-shell, backup and restore for docker now check if all required variables are set

--- scripts/docker/restore.bash
+++ scripts/docker/restore.bash
@@ -11,31 +11,49 @@ echo
 DUMP_NAME=$1
 BACKUPS_DIR=backups
 
-if [ -z "$DUMP_NAME" ]; then
-  # restore latest
-  DUMP_NAME=$BACKUPS_DIR/$(ls -t1 $BACKUPS_DIR/ | head -n 1)
+# check if all neccesary information is given
+if
+  [ ! -n "$DB_DRIVER" ] ||
+    [ ! -n "$DB_USER" ] ||
+    [ ! -n "$DB_SERVER" ] ||
+    [ ! -n "$DB_DATABASE" ] ||
+    [ ! -n "$DB_PASSWORD" ]
+then
   echo
-  echo "  → ${BOLD}Restoring latest dump${NC}"
-  echo "    $DUMP_NAME"
+  echo "  ${RED}ERROR${NC} Some information is missing in your ${WHITE}.env${NC} file (@see below)."
+  echo
+  echo "  → DB_DRIVER=$DB_DRIVER"
+  echo "  → DB_USER=$DB_USER"
+  echo "  → DB_SERVER=$DB_SERVER"
+  echo "  → DB_DATABASE=$DB_DATABASE"
+  echo "  → DB_PASSWORD=$DB_PASSWORD"
 else
-  # restore from DUMP_NAME
+  if [ -z "$DUMP_NAME" ]; then
+    # restore latest
+    DUMP_NAME=$BACKUPS_DIR/$(ls -t1 $BACKUPS_DIR/ | head -n 1)
+    echo
+    echo "  → ${BOLD}Restoring latest dump${NC}"
+    echo "    $DUMP_NAME"
+  else
+    # restore from DUMP_NAME
+    echo
+    echo "  → ${BOLD}Restoring dump${NC}"
+    echo "    $DUMP_NAME"
+  fi
+
+  # create temporary copy from DUMP_NAME
+  gzip -d -c $DUMP_NAME >temp.sql.tmp
+
+  # import
+  docker exec -i $(docker-compose ps -q db) $DB_DRIVER -u$DB_USER -p$DB_PASSWORD -h$DB_SERVER $DB_DATABASE <temp.sql.tmp
+
+  # remove temporary files
+  rm temp.sql.tmp
+
   echo
-  echo "  → ${BOLD}Restoring dump${NC}"
-  echo "    $DUMP_NAME"
+  echo "  ${GREEN}SUCCESS${NC} Done"
+  echo
 fi
-
-# create temporary copy from DUMP_NAME
-gzip -d -c $DUMP_NAME >temp.sql.tmp
-
-# import
-docker exec -i $(docker-compose ps -q db) $DB_DRIVER -u$DB_USER -p$DB_PASSWORD -h$DB_SERVER $DB_DATABASE <temp.sql.tmp
-
-# remove temporary files
-rm temp.sql.tmp
-
-echo
-echo "  ${GREEN}SUCCESS${NC} Done"
-echo
 
 # more-make-up
 MORE_MAKE_UP="${0/make-up/more-make-up}"
