a5b8c8f85547d22f3f9cf4976892b6d0a435250e
Joachim Schole <jschole@fork.de>
2020-08-28 15:25:05 +0200

    check ssh status only once

--- scripts/sync/staging-to-dev.bash
+++ scripts/sync/staging-to-dev.bash
@@ -8,132 +8,126 @@ echo
 echo "$I18N_TASK Synchronize database and uploads from environment ${GREEN}staging${NC} to ${WHITE}dev${NC}"
 echo
 
-echo
-echo "$I18N_TASK Get database from ${GREEN}staging${NC}"
-echo
-
 E_STAGING_DB_DUMP_NAME=staging.sql.gz
 
 # check if all neccesary information is given
 if
   [ ! -n "$E_STAGING_SSH_USER" ] ||
-    [ ! -n "$E_STAGING_SSH_HOST" ] ||
-    [ ! -n "$E_STAGING_DB_USER" ] ||
-    [ ! -n "$E_STAGING_DB_PASS" ] ||
-    [ ! -n "$E_STAGING_DB_NAME" ] ||
-    [ ! -n "$E_STAGING_DB_DUMP_NAME" ] ||
-    [ ! -n "$E_STAGING_PROJECT_HOME" ]
+	[ ! -n "$E_STAGING_SSH_HOST" ]
 then
   echo
   echo "$I18N_ERROR Some information is missing in your ${WHITE}.env${NC} file (@see below)."
   echo
   echo "  → E_STAGING_SSH_USER=$E_STAGING_SSH_USER"
   echo "  → E_STAGING_SSH_HOST=$E_STAGING_SSH_HOST"
-  echo "  → E_STAGING_DB_USER=$E_STAGING_DB_USER"
-  echo "  → E_STAGING_DB_PASS=$E_STAGING_DB_PASS"
-  echo "  → E_STAGING_DB_NAME=$E_STAGING_DB_NAME"
-  echo "  → E_STAGING_DB_DUMP_NAME=$E_STAGING_DB_DUMP_NAME"
-  echo "  → E_STAGING_PROJECT_HOME=$E_STAGING_PROJECT_HOME"
 else
-  echo "$I18N_INFO Credentials found in $ENV_FILE"
-  echo 
-  echo "  → E_STAGING_SSH_USER=$E_STAGING_SSH_USER"
-  echo "  → E_STAGING_SSH_HOST=$E_STAGING_SSH_HOST"
-  echo "  → E_STAGING_DB_USER=$E_STAGING_DB_USER"
-  echo "  → E_STAGING_DB_PASS=$E_STAGING_DB_PASS"
-  echo "  → E_STAGING_DB_NAME=$E_STAGING_DB_NAME"
-  echo "  → E_STAGING_DB_DUMP_NAME=$E_STAGING_DB_DUMP_NAME"
-  echo "  → E_STAGING_PROJECT_HOME=$E_STAGING_PROJECT_HOME"
-  echo 
+	echo "$I18N_INFO Credentials found in $ENV_FILE"
+	echo
+	echo "  → E_STAGING_SSH_USER=$E_STAGING_SSH_USER"
+	echo "  → E_STAGING_SSH_HOST=$E_STAGING_SSH_HOST"
 
-    # test ssh connection
-  status=$(ssh -o BatchMode=yes -o ConnectTimeout=5 $E_STAGING_SSH_USER@$E_STAGING_SSH_HOST echo ok 2>&1)
+	# test ssh connection
+	status=$(ssh -o BatchMode=yes -o ConnectTimeout=5 $E_STAGING_SSH_USER@$E_STAGING_SSH_HOST echo ok 2>&1)
 
-  if [ "$status" = "ok" ]; then
-    # dump database
-    if [[ -z $E_STAGING_DOCKER_COMPOSE_FILE || -z $E_STAGING_DOCKER_COMPOSE_SERVICE ]]; then
-      echo "$I18N_INFO Dumping from mysql"
-      ssh -T "$E_STAGING_SSH_USER@$E_STAGING_SSH_HOST" 'mysqldump -u' "$E_STAGING_DB_USER"' -p'"$E_STAGING_DB_PASS"' '"$E_STAGING_DB_NAME"' | gzip -f > '"$E_STAGING_PROJECT_HOME"'/'"$E_STAGING_DB_DUMP_NAME"
-    else
-      echo "$I18N_INFO Dumping from docker container mysql"
-      ssh -T "$E_STAGING_SSH_USER@$E_STAGING_SSH_HOST" 'docker-compose -f '"$E_STAGING_DOCKER_COMPOSE_FILE"' exec -T '"$E_STAGING_DOCKER_COMPOSE_SERVICE"' mysqldump -u'"$E_STAGING_DB_USER"' -p'\""$E_STAGING_DB_PASS"\"' '"$E_STAGING_DB_NAME"' | gzip -f > '"$E_STAGING_PROJECT_HOME"'/'"$E_STAGING_DB_DUMP_NAME"''
-    fi
+	if [ "$status" = "ok" ]; then
+		echo
+		echo "$I18N_TASK Get database from ${GREEN}staging${NC}"
+		echo
 
-    # copy database dump from staging to dev
-    scp "$E_STAGING_SSH_USER@$E_STAGING_SSH_HOST":$E_STAGING_PROJECT_HOME/$E_STAGING_DB_DUMP_NAME $E_STAGING_DB_DUMP_NAME
+		# check if all neccesary information is given
+		if
+				[ ! -n "$E_STAGING_DB_USER" ] ||
+				[ ! -n "$E_STAGING_DB_PASS" ] ||
+				[ ! -n "$E_STAGING_DB_NAME" ] ||
+				[ ! -n "$E_STAGING_DB_DUMP_NAME" ] ||
+				[ ! -n "$E_STAGING_PROJECT_HOME" ]
+		then
+			echo
+			echo "$I18N_ERROR Some information is missing in your ${WHITE}.env${NC} file (@see below)."
+			echo
+			echo "  → E_STAGING_DB_USER=$E_STAGING_DB_USER"
+			echo "  → E_STAGING_DB_PASS=$E_STAGING_DB_PASS"
+			echo "  → E_STAGING_DB_NAME=$E_STAGING_DB_NAME"
+			echo "  → E_STAGING_DB_DUMP_NAME=$E_STAGING_DB_DUMP_NAME"
+			echo "  → E_STAGING_PROJECT_HOME=$E_STAGING_PROJECT_HOME"
+		else
+			echo "$I18N_INFO Credentials found in $ENV_FILE"
+			echo
+			echo "  → E_STAGING_DB_USER=$E_STAGING_DB_USER"
+			echo "  → E_STAGING_DB_PASS=$E_STAGING_DB_PASS"
+			echo "  → E_STAGING_DB_NAME=$E_STAGING_DB_NAME"
+			echo "  → E_STAGING_DB_DUMP_NAME=$E_STAGING_DB_DUMP_NAME"
+			echo "  → E_STAGING_PROJECT_HOME=$E_STAGING_PROJECT_HOME"
+			echo
 
-    # remove dump from staging
-    ssh -T "$E_STAGING_SSH_USER@$E_STAGING_SSH_HOST" 'rm' "$E_STAGING_PROJECT_HOME"'/'"$E_STAGING_DB_DUMP_NAME"
+			# dump database
+			if [[ -z $E_STAGING_DOCKER_COMPOSE_FILE || -z $E_STAGING_DOCKER_COMPOSE_SERVICE ]]; then
+				echo "$I18N_INFO Dumping from mysql"
+				ssh -T "$E_STAGING_SSH_USER@$E_STAGING_SSH_HOST" 'mysqldump -u' "$E_STAGING_DB_USER"' -p'"$E_STAGING_DB_PASS"' '"$E_STAGING_DB_NAME"' | gzip -f > '"$E_STAGING_PROJECT_HOME"'/'"$E_STAGING_DB_DUMP_NAME"
+			else
+				echo "$I18N_INFO Dumping from docker container mysql"
+				ssh -T "$E_STAGING_SSH_USER@$E_STAGING_SSH_HOST" 'docker-compose -f '"$E_STAGING_DOCKER_COMPOSE_FILE"' exec -T '"$E_STAGING_DOCKER_COMPOSE_SERVICE"' mysqldump -u'"$E_STAGING_DB_USER"' -p'\""$E_STAGING_DB_PASS"\"' '"$E_STAGING_DB_NAME"' | gzip -f > '"$E_STAGING_PROJECT_HOME"'/'"$E_STAGING_DB_DUMP_NAME"''
+			fi
 
-    if [ -f "$E_STAGING_DB_DUMP_NAME" ]; then
-      # restore database
-      $my_dir/../backup/restore.bash $E_STAGING_DB_DUMP_NAME
+			# copy database dump from staging to dev
+			scp "$E_STAGING_SSH_USER@$E_STAGING_SSH_HOST":$E_STAGING_PROJECT_HOME/$E_STAGING_DB_DUMP_NAME $E_STAGING_DB_DUMP_NAME
 
-      # remove dump
-      rm $E_STAGING_DB_DUMP_NAME
+			# remove dump from staging
+			ssh -T "$E_STAGING_SSH_USER@$E_STAGING_SSH_HOST" 'rm' "$E_STAGING_PROJECT_HOME"'/'"$E_STAGING_DB_DUMP_NAME"
 
-      echo
-      echo "$I18N_SUCCESS Synchronized database"
-      echo
-    else
-      echo
-      echo "$I18N_ERROR No file found that could be used to restore database"
-      echo
-    fi
-  else
-    echo
-    echo "$I18N_ERROR SSH connection could not be established"
-    echo
-  fi
+			if [ -f "$E_STAGING_DB_DUMP_NAME" ]; then
+				# restore database
+				$my_dir/../backup/restore.bash $E_STAGING_DB_DUMP_NAME
 
-fi
+				# remove dump
+				rm $E_STAGING_DB_DUMP_NAME
 
-echo
-echo "$I18N_TASK Synchronize uploads"
-echo
+				echo
+				echo "$I18N_SUCCESS Synchronized database"
+				echo
+			else
+				echo
+				echo "$I18N_ERROR No file found that could be used to restore database"
+				echo
+			fi
+		fi
 
-# check if all neccesary information is given
-if
-  [ ! -n "$E_STAGING_SSH_USER" ] ||
-    [ ! -n "$E_STAGING_SSH_HOST" ] ||
-    [ ! -n "$E_STAGING_UPLOADS" ] ||
-    [ ! -n "$E_DEV_UPLOADS" ]
-then
-  echo
-  echo "$I18N_ERROR Some information is missing in your ${WHITE}.env${NC} file (@see below)."
-  echo
-  echo "  → E_STAGING_SSH_USER=$E_STAGING_SSH_USER"
-  echo "  → E_STAGING_SSH_HOST=$E_STAGING_SSH_HOST"
-  echo "  → E_STAGING_UPLOADS=$E_STAGING_UPLOADS"
-  echo "  → E_DEV_UPLOADS=$E_DEV_UPLOADS"
-else
-  echo "$I18N_INFO Credentials found in $ENV_FILE"
-  echo 
-  echo "  → E_STAGING_SSH_USER=$E_STAGING_SSH_USER"
-  echo "  → E_STAGING_SSH_HOST=$E_STAGING_SSH_HOST"
-  echo "  → E_STAGING_UPLOADS=$E_STAGING_UPLOADS"
-  echo "  → E_DEV_UPLOADS=$E_DEV_UPLOADS"
-  echo 
+		echo
+		echo "$I18N_TASK Synchronize uploads"
+		echo
 
-  # test ssh connection
-  status=$(ssh -o BatchMode=yes -o ConnectTimeout=5 $E_STAGING_SSH_USER@$E_STAGING_SSH_HOST echo ok 2>&1)
+		# check if all neccesary information is given
+		if
+				[ ! -n "$E_STAGING_UPLOADS" ] ||
+				[ ! -n "$E_DEV_UPLOADS" ]
+		then
+			echo
+			echo "$I18N_ERROR Some information is missing in your ${WHITE}.env${NC} file (@see below)."
+			echo
+			echo "  → E_STAGING_UPLOADS=$E_STAGING_UPLOADS"
+			echo "  → E_DEV_UPLOADS=$E_DEV_UPLOADS"
+		else
+			echo "$I18N_INFO Credentials found in $ENV_FILE"
+			echo
+			echo "  → E_STAGING_UPLOADS=$E_STAGING_UPLOADS"
+			echo "  → E_DEV_UPLOADS=$E_DEV_UPLOADS"
+			echo
 
-  if [ "$status" = "ok" ]; then
-    # create dir
-    mkdir -p $E_DEV_UPLOADS
+			# create dir
+			mkdir -p $E_DEV_UPLOADS
 
-    # sync uploads
-    rsync -F -L -a -z -e ssh $E_STAGING_SSH_USER@$E_STAGING_SSH_HOST:$E_STAGING_UPLOADS/* $E_DEV_UPLOADS --delete-after --progress
+			# sync uploads
+			rsync -F -L -a -z -e ssh $E_STAGING_SSH_USER@$E_STAGING_SSH_HOST:$E_STAGING_UPLOADS/* $E_DEV_UPLOADS --delete-after --progress
 
-    echo
-    echo "$I18N_SUCCESS Synchronized uploads to ./$E_DEV_UPLOADS (There may be additional output above)"
-    echo
-  else
-    echo
-    echo "$I18N_ERROR SSH connection could not be established"
-    echo
-  fi
-  
+			echo
+			echo "$I18N_SUCCESS Synchronized uploads to ./$E_DEV_UPLOADS (There may be additional output above)"
+			echo
+		fi
+	else
+		echo
+		echo "$I18N_ERROR SSH connection could not be established"
+		echo
+	fi
 fi
 
 # more-make-up
